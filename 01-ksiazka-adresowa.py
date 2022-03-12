dc85425b1de892853af92c2a49c27e6228d23c59
Paweł Walner <pawel@walner.pl>
2022-03-12 13:42:59 +0100

    kolejny update

--- 01-ksiazka-adresowa.py
+++ 01-ksiazka-adresowa.py
@@ -9,7 +9,11 @@
 # - Program powinien na starcie mieć już wprowadzone kilka wpisów.
 
 
-print('Tu Twoja ksiażka adresowa. Podaj komendę, aby skorzystać z moich funkcji: \n  1. show_all <- Pokażę Ci wszystkie Twoje kontakty \n 2. create_new <- Stworzę nowy kontakt \n 3. remove')
+print('Tu Twoja ksiażka adresowa. Podaj komendę, aby skorzystać z moich funkcji: \n '
+      '1. show_all <- Pokażę Ci wszystkie Twoje kontakty \n '
+      '2. create_new <- Stworzę nowy kontakt \n '
+      '3. remove_contact <- Usunę wybrany kontakt \n '
+      '4. exit_book <- Pożegnamy się tu i teraz.')
 
 address_book = [
     [1, 'Paweł', 'Walner', '+48600200300', 'test5@wp.pl'],
@@ -20,7 +24,7 @@ address_book = [
 ]
 
 
-# wyświetlanie wszystkich wpisów:
+# Wyświetlanie wszystkich wpisów:
 for element in address_book:
     print(f'ID: {element[0]} Imię: {element[1]} Nazwisko: {element[2]} Telefon: {element[3]} Email: {element[4]}')
     print('--- --- --- --- ---')
@@ -36,20 +40,21 @@ new_contact = [last_id + 1, first_name, last_name, phone, email]
 address_book.append(new_contact)
 print(f'Kontakt {first_name} {last_name} został pomyślnie dodany do Twojej listy kontaktów')
 
-# usunięcie wpisu
+# Usunięcie kontaktu
 print('Poniżej podaję listę kontaktów wraz z numerem ID')
 for element in address_book:
     print(f'{element[1]} {element[2]} - numer ID: {element[0]}')
 
 
 remove_id = int(input('Podaj numer ID kontaktu, który chcesz usunąć:'))
-index_entries_in_address_book = address_book[-1][0] - 1
-print(index_entries_in_address_book)
+entries_counter = address_book[-1][0]
 
-if remove_id - 1 in range(index_entries_in_address_book):
+
+if remove_id - 1 in range(entries_counter):
     first_name_to_remove = address_book[remove_id - 1][1]
     last_name_to_remove = address_book[remove_id - 1][2]
     print(f'Kontakt {first_name_to_remove} {last_name_to_remove} został usunięty z Twojej książki adresowej')
     address_book.pop(remove_id - 1)
 else:
     print('Podałeś ID, które nie znajduje się na liście - żaden z kontaktów nie został usunięty.')
+    
